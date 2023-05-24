18d0042549480ada91bfedbe32a593d9a9ffe7ec
Dave Cottlehuber <dch@skunkwerks.at>
2023-05-24 12:31:41 +0000

    add downcase

--- lib/wat.ex
+++ lib/wat.ex
@@ -1,5 +1,14 @@
 defmodule Wat do
   def main(_) do
-    IO.puts("wat")
+    "WAT" |> downcase() |> IO.puts()
   end
+
+  def downcase(bin), do: downcase(bin, << >>)
+
+  def downcase(<<h, t::binary>>, acc) when h in ?A..?Z,
+    do: downcase(t, <<acc::binary, h + 32>>)
+
+  def downcase(<<h, t::binary>>, acc), do: downcase(t, <<acc::binary, h>>)
+  def downcase(<<>>, acc), do: acc
+
 end
